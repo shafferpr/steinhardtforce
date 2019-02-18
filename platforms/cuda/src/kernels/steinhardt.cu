bdb4faeff292883eac72266b01d0abe0c1d23967
Patrick Shaffer <shafferpr@gmail.com>
2019-02-17 19:34:48 -0500

    made some changes to steinhardt.cu to facilitate using a different value of l in the legendgre polynomial

--- platforms/cuda/src/kernels/steinhardt.cu
+++ platforms/cuda/src/kernels/steinhardt.cu
@@ -4,6 +4,29 @@
 /**
  * Perform the first step of computing q6.
  */
+
+
+__device__ real legendre(real rdot,int steinhardt_order){
+	   real result=0;
+
+	   if(steinhardt_order == 6)
+	   		       result= (231*powf(rdot,6.0)-315*powf(rdot,4.0)+105*powf(rdot,2.0)-5)/16;
+	   else if (steinhardt_order == 4)
+	   	result=(35*powf(rdot,4.0)-30*powf(rdot,2.0)+3)/8;
+
+	return result;
+}
+
+__device__ real legendre_deriv(real rdot, int steinhardt_order){
+	   real result=0;
+	   if(steinhardt_order==6)
+	   	result=(1386*powf(rdot,5.0)-1260*powf(rdot,3)+210*rdot)/16;
+       	else if(steinhardt_order == 4)
+		result=(140*powf(rdot,3.0)-60*rdot)/8;
+
+return result;
+}
+
 extern "C" __global__ void computeSteinhardt(int numParticles, const real4* __restrict__ posq,
          const int* __restrict__ particles, real* buffer, unsigned long long* __restrict__ forceBuffers, int paddedNumAtoms, real4 periodicBoxSize, real4 invPeriodicBoxSize,
                  real4 periodicBoxVecX, real4 periodicBoxVecY, real4 periodicBoxVecZ,real* __restrict__ M, real* __restrict__ N, real* __restrict__ F) {
@@ -23,7 +46,7 @@ extern "C" __global__ void computeSteinhardt(int numParticles, const real4* __re
           real3 positionj=trimTo3(posq[particles[j]]);
           real3 rij= make_real3(positioni.x-positionj.x, positioni.y-positionj.y, positioni.z-positionj.z);
           APPLY_PERIODIC_TO_DELTA(rij);
-          real rij_norm=powf(rij.x*rij.x + rij.y*rij.y + rij.z*rij.z,0.5);
+          real rij_norm=sqrtf(rij.x*rij.x + rij.y*rij.y + rij.z*rij.z);
 	  if(rij_norm<1.6*CUTOFF){
 	  real rij_pow6=powf((rij_norm-CUTOFF)/1,6);
           real switch_ij=(1-rij_pow6)/(1-powf(rij_pow6,2));
@@ -34,12 +57,13 @@ extern "C" __global__ void computeSteinhardt(int numParticles, const real4* __re
               real3 positionk=trimTo3(posq[particles[k]]);
               real3 rik= make_real3(positioni.x-positionk.x, positioni.y-positionk.y, positioni.z-positionk.z);
               APPLY_PERIODIC_TO_DELTA(rik);
-              real rik_norm=powf(rik.x*rik.x + rik.y*rik.y + rik.z*rik.z,0.5);
+              real rik_norm=sqrtf(rik.x*rik.x + rik.y*rik.y + rik.z*rik.z);
 	      if(rik_norm<1.6*CUTOFF){
 	      real rik_pow6=powf((rik_norm-CUTOFF)/1,6);
               real switch_ik=(1-rik_pow6)/(1-powf(rik_pow6,2));
               real rdot = (rij.x*rik.x + rij.y*rik.y + rij.z*rik.z)/(rik_norm*rij_norm);
-              real P6=(231*powf(rdot,6.0)-315*powf(rdot,4.0)+105*powf(rdot,2.0)-5)/16;
+              //real P6=(231*powf(rdot,6.0)-315*powf(rdot,4.0)+105*powf(rdot,2.0)-5)/16;
+	      real P6=legendre(rdot, 6);
               //M[i] += P6*switch_ik*switch_ij;
               sumM += P6*switch_ik*switch_ij;
             }
@@ -64,21 +88,21 @@ extern "C" __global__ void computeSteinhardtForces(int numParticles, const real4
                  real4 periodicBoxVecX, real4 periodicBoxVecY, real4 periodicBoxVecZ,real* __restrict__ M, real* __restrict__ N, real* __restrict__ F, real Q_tot) {
 
     unsigned int i = blockIdx.x*blockDim.x+threadIdx.x;
-    real prefactor=powf(4*3.14159/13,0.5)/numParticles;
+    real prefactor=sqrtf(4*3.14159/13)/numParticles;
     while(i < numParticles){
     	//printf("%d\n", i);
 	//printf("Hello from block %d, thread %d\n", blockIdx.x, threadIdx.x);
         real3 positioni=trimTo3(posq[particles[i]]);
 	
 	real3 Ficomp=make_real3(0);
-	real M_prefactor=-1/(2*N[i]*powf(M[i],0.5));
+	real M_prefactor=-1/(2*N[i]*sqrtf(M[i]));
 
         for(int j=0; j<numParticles; j++){
             if( j!=i ){
                 real3 positionj=trimTo3(posq[particles[j]]);
                 real3 rij= make_real3(positioni.x-positionj.x, positioni.y-positionj.y, positioni.z-positionj.z);
                 APPLY_PERIODIC_TO_DELTA(rij);
-                real rij_norm=powf(rij.x*rij.x + rij.y*rij.y + rij.z*rij.z,0.5);
+                real rij_norm=sqrtf(rij.x*rij.x + rij.y*rij.y + rij.z*rij.z);
 		if(rij_norm < 2.0*CUTOFF){
                 real3 delta_rij_norm=rij/(2*rij_norm);
 		real rij_pow6=powf((rij_norm-CUTOFF)/1,6);
@@ -94,7 +118,7 @@ extern "C" __global__ void computeSteinhardtForces(int numParticles, const real4
                         real3 rik= make_real3(positioni.x-positionk.x, positioni.y-positionk.y, positioni.z-positionk.z);
                         APPLY_PERIODIC_TO_DELTA(rik);
 			
-                        real rik_norm=powf(rik.x*rik.x + rik.y*rik.y + rik.z*rik.z,0.5);
+                        real rik_norm=sqrtf(rik.x*rik.x + rik.y*rik.y + rik.z*rik.z);
 			if(rik_norm < 2.0*CUTOFF){
                         real3 delta_rik_norm=rik/(2*rik_norm);
 			real rik_pow6=powf((rik_norm-CUTOFF)/1,6);
@@ -105,8 +129,10 @@ extern "C" __global__ void computeSteinhardtForces(int numParticles, const real4
                         real delta_switch_ik=(6*rik_pow5*switch_ik_denominator-12*rik_pow5*rik_pow6*switch_ik_numerator)/(switch_ik_denominator*switch_ik_denominator);
 			
                         real rdot = rij.x*rik.x + rij.y*rik.y + rij.z*rik.z/(rij_norm*rik_norm);
-                        real P6=(231*powf(rdot,6.0)-315*powf(rdot,4.0)+105*powf(rdot,2.0)-5)/16;
-                        real delta_P6=(1386*powf(rdot,5.0)-1260*powf(rdot,3)+210*rdot)/16;
+                        //real P6=(231*powf(rdot,6.0)-315*powf(rdot,4.0)+105*powf(rdot,2.0)-5)/16;
+			real P6=legendre(rdot,6);
+                        //real delta_P6=(1386*powf(rdot,5.0)-1260*powf(rdot,3)+210*rdot)/16;
+			real delta_P6=legendre_deriv(rdot,6);
 			real3 delta_rijj= -2*delta_rij_norm + make_real3(2*delta_rij_norm.x*delta_rij_norm.x*delta_rik_norm.x,2*delta_rij_norm.y*delta_rij_norm.y*delta_rik_norm.y,2*delta_rij_norm.z*delta_rij_norm.z*delta_rik_norm.z)/rij_norm;
 			real3 delta_rikk= -2*delta_rik_norm + make_real3(2*delta_rik_norm.x*delta_rik_norm.x*delta_rij_norm.x,2*delta_rik_norm.y*delta_rik_norm.y*delta_rij_norm.y,2*delta_rik_norm.z*delta_rik_norm.z*delta_rij_norm.z)/rik_norm;
 			real3 delta_rijik = -delta_rijj-delta_rikk;
